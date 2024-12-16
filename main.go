2c7d7acdbf7878b95410618eb9529452a089ad85
上杉夏香 <30595874+ZMbiubiubiu@users.noreply.github.com>
2024-12-16 10:34:20 +0800

    Update main.go

--- main.go
+++ main.go
@@ -26,28 +26,8 @@ func init() {
 	}
 }
 
-func AddLogo() {
-	logoFile := "/Users/zhangmeng56/Documents/github/go-practice/pdf/compress-pdf-with-pdfium-main/convert.png"
-	files := []string{
-		"../pdf-files/cbook.pdf",
-		// "../pdf_files/nologo.pdf",
-	}
-	var err error
-
-	for _, file := range files {
-		err = PDFAddLogoV1(context.Background(), instance, logoFile, file, strings.Replace(file, ".pdf", "_logo_v1.pdf", 1), 1)
-		if err != nil {
-			log.Fatalf("PDFAddLogoV1 添加logo失败: %v", err)
-		}
-		err = PDFAddLogoV2(context.Background(), instance, logoFile, file, strings.Replace(file, ".pdf", "_logo_v2.pdf", 1), 1)
-		if err != nil {
-			log.Fatalf("PDFAddLogoV2 添加logo失败: %v", err)
-		}
-	}
-}
-
 func CompressPDF() {
-	inputPath := "../pdf-files/coding.pdf"
+	inputPath := "../pdf-files/cbook1.pdf"
 
 	if err := CompressImagesInPlace(instance, inputPath, 90); err != nil {
 		log.Fatalf("压缩 PDF 失败: %v", err)
@@ -58,7 +38,6 @@ func main() {
 	beginTime := time.Now()
 
 	CompressPDF()
-	// AddLogo()
 
 	fmt.Printf("PDF 压缩成功，耗时: %dms\n", time.Since(beginTime).Milliseconds())
 }
